ed62bf072ef58e4f90e2ea017b3ea94d9d313bb3
Ali <ali.zolghadr@gmail.com>
2023-03-19 12:30:16 +1100

    functions added to create photo table, and input data for photo table

--- flickr.py
+++ flickr.py
@@ -7,6 +7,7 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
+import sqlite3
 
 
 def main():
@@ -33,6 +34,10 @@ def main():
     model = ResNet50(weights='imagenet')
     tags_probs = tag_all_photos_Resnet50(model, "flickr_photo_dl/", img_set, 3)
     print(tags_probs)
+
+    conn = sqlite3.connect("flickr.db")
+
+
 def all_flickr_api_calls(base_url, method, params, n_photos, max_photo_per_call):
     all_flickr_response_json_list = []
     url = base_url + method
@@ -50,6 +55,7 @@ def single_flickr_api_call(url, params):
     response = requests.get(url, params=params)
     return response
 
+
 def tag_all_photos_Resnet50(model, path, img_set, n_tags):
     tags_probs = []
     for img in img_set:
@@ -58,9 +64,10 @@ def tag_all_photos_Resnet50(model, path, img_set, n_tags):
         tag_probs = tag_single_photo_ResNet50(model, img_path, n_tags)
         tags_probs.append({
             "photo_id": img_id,
-            "tag_probs":tag_probs
+            "tag_probs": tag_probs
         })
     return tags_probs
+
 
 def tag_single_photo_ResNet50(model, img_path, n_tag):
     img = image.load_img(img_path, target_size=(224, 224))
@@ -73,14 +80,12 @@ def tag_single_photo_ResNet50(model, img_path, n_tag):
     # (one such list for each sample in the batch)
     decoded_preds = decode_predictions(preds, top=n_tag)[0]
     tags_probs = []
-    encoding = "utf-8"
     for i in range(len(decoded_preds)):
         tag = str(decoded_preds[i][1])
         prob = decoded_preds[i][2]
         tags_probs.append((tag, prob))
 
     return tags_probs
-
 
 
 def flickr_response_to_json(flickr_response):
@@ -108,6 +113,48 @@ def dl_flickr_photos(all_flickr_response_list):
                 f.write(img_data)
             img_set.add(name)
     return img_set
+
+
+def create_photo_table():
+    conn = sqlite3.connect("flickr.db")
+    c = conn.cursor()
+    c.execute("""CREATE TABLE photos(
+        photo_id TEXT,
+        owner_id TEXT,
+        views INTEGER,
+        url_o TEXT,
+        url_q TEXT
+        )""")
+
+
+def create_input_for_photo_table(all_flickr_response_list):
+    input_list = []
+    n_lists = len(all_flickr_response_list)
+    for i in range(n_lists):
+        for photo in all_flickr_response_list[i]["photos"]["photo"]:
+            try:
+                photo_input = (
+                    photo["id"],
+                    photo["owner"],
+                    int(photo["views"]),
+                    photo["url_o"],
+                    photo["url_q"]
+                )
+            except KeyError:
+                photo_input = (
+                    photo["id"],
+                    photo["owner"],
+                    int(photo["views"]),
+                    None,
+                    photo["url_q"]
+                )
+            input_list.append(photo_input)
+    return input_list
+
+
+def add_photos_to_photo_table():
+
+    return
 
 def get_api_key():
     try:
